de5e1d6df26f00a8024185696153b0f2b689051f
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-10 18:25:32 +0300

    finish logic for only a single sequence

--- script.js
+++ script.js
@@ -1,14 +1,66 @@
 // Initialising DOM Elements
-
 const title = document.getElementById("level-title");
+const btns = Array.from(document.querySelectorAll(".btn"));
 
-const greenBtn = document.getElementById("green");
-const redBtn = document.getElementById("red");
-const yellowBtn = document.getElementById("yellow");
-const blueBtn = document.getElementById("blue");
+const state = {
+  level: 0,
+  isPlaying: false,
+  choosenBtn: null,
+  sequence: [],
+};
+
+const resetState = () => {
+  state.level = 0;
+  state.isPlaying = false;
+  state.sequence = [];
+};
+
+const newPlay = () => {
+  const curr = currentPlay();
+  const choosenBtn = btns[curr];
+  state.choosenBtn = choosenBtn;
+};
+
+const showCurrentPlay = () => {
+  newPlay();
+  const choosenBtn = state.choosenBtn;
+  choosenBtn.classList.add("pressed");
+  setTimeout(() => {
+    choosenBtn.classList.remove("pressed");
+  }, 1000);
+};
+
+const checkAnswer = (event) => {
+  if (!state.isPlaying) {
+    return;
+  }
+
+  console.log("event:", event.target);
+
+  if (state.choosenBtn === event.target) {
+    state.level++;
+    title.innerText = `Level ${state.level}`;
+    showCurrentPlay();
+  } else {
+    title.innerText = `Game Over, You lost in level ${state.level} !! Press any key to start again`;
+    resetState();
+  }
+};
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", checkAnswer);
+});
+
+// btns.
 
 const startGame = () => {
-  title.innerText = "Level 1";
+  if (state.isPlaying) {
+    return;
+  }
+
+  state.level += 1;
+  title.innerText = `Level ${state.level}`;
+  state.isPlaying = true;
   game();
 };
 
@@ -16,30 +68,12 @@ const startGame = () => {
 document.addEventListener("keypress", startGame);
 
 const currentPlay = () => {
-  return Math.floor((Math.random() * 10) % 4);
+  const curr = Math.floor((Math.random() * 10) % 4);
+  state.sequence.push(curr);
+  return curr;
 };
 
-const state = {
-  buttons: {
-    0: greenBtn,
-    1: redBtn,
-    2: yellowBtn,
-    3: blueBtn,
-  },
-  isPlaying: true,
-};
-
-const curr = currentPlay();
-const choosenBtn = state.buttons[curr];
-
-console.log(curr, choosenBtn);
-
-const showCurrentPlay = () => {
-  choosenBtn.classList.add("pressed");
-  setTimeout(() => {
-    choosenBtn.classList.remove("pressed");
-  }, 1000);
-};
+// console.log(curr, choosenBtn);
 
 const game = () => {
   showCurrentPlay();
